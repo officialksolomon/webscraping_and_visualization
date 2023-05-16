bc1914ecd5eff553cbd8937ca31e52be9bf49f43
officialksolomon <solomonuche42@gmail.com>
2023-05-16 10:37:05 +0100

    updated visualization logic and error handling.

--- visualization.py
+++ visualization.py
@@ -6,7 +6,11 @@ import matplotlib.pyplot as plt
 class DataVisualizer(ABC):
     @abstractmethod
     def get_data(self, file_name):
-       pass
+        pass
+
+    @abstractmethod
+    def validate_data_value(self, data):
+        pass
 
     @abstractmethod
     def visualize_data(self, data):
@@ -16,30 +20,38 @@ class DataVisualizer(ABC):
 
 class PieChartVisualizer(DataVisualizer):
     def __init__(self) -> None:
-        self.data  = None
+        self.data = None
         self._data_columns = None
-    
 
     def get_data(self, file_name):
         dataframe = pd.read_csv(file_name)
         self.data_columns = list(dataframe.head(0).columns)
         self.data = dataframe.to_numpy()[0]
 
+    def validate_data_value(self, data):
+        if type(int(data)) not in [int, float]:
+            raise ValueError("Invalid Value. Pie Chart requires int or float")
+        return data
+        
+
     def visualize_data(self):
+        
         fig, ax = plt.subplots()
         if self.data is not None:
-            ax.pie([data.replace(",", "") for data in self.data], labels=self.data_columns)
-            ax.set_title('Coronavirus cases, deaths, and recovered cases worldwide.')
-            fig.savefig('coronavirus_data.png')
+            ax.pie(
+                [self.validate_data_value(data.replace(",", "")) for data in self.data],
+                labels=self.data_columns,
+            )
+            ax.set_title(
+                "Coronavirus cases, deaths, and recovered cases worldwide."
+            )
+            fig.savefig("coronavirus_data.png")
             plt.show()
 
-         
 
 if __name__ == "__main__":
-    visualizer =  PieChartVisualizer()
-    visualizer.get_data('coronavirus_data.csv')
+    visualizer = PieChartVisualizer()
+    visualizer.get_data("coronavirus_data.csv")
     print(visualizer.data)
     print(visualizer.data_columns)
     visualizer.visualize_data()
-
-    
